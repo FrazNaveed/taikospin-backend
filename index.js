702a163dd357495114d4dad3662e4a06a8c147df
Fraz Naveed <01-134181-021@student.bahria.edu.pk>
2025-02-17 20:58:44 +0500

    feat: added body-parser

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Import packages
 const express = require("express");
 const cors = require("cors"); // Import CORS
+const bodyParser = require("body-parser"); // Import body-parser
 const authRoutes = require("./routes/auth");
 
 // Initialize app
 const app = express();
 
 // Middlewares
-app.use(express.json());
 app.use(cors()); // Enable CORS
+app.use(bodyParser.json()); // Parse JSON request bodies
+app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 
 // Routes
 app.use("/auth", authRoutes);
