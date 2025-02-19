7ecd5d41858deff4d82f5d46ce5a34f90d596661
Fraz Naveed <01-134181-021@student.bahria.edu.pk>
2025-02-19 05:54:58 +0500

    feat: cors changes

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const contractRoutes = require("./routes/contract");
 const app = express();
 
 // Middlewares
-app.use(cors()); // Enable CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow any domain
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(bodyParser.json()); // Parse JSON request bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 
