0dbbdab9e5b5c20a9d3e938b292d4044204c4fdd
Fraz Naveed <01-134181-021@student.bahria.edu.pk>
2025-02-19 06:14:13 +0500

    fix: cors changed

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
 const contractRoutes = require("./routes/contract");
 
+const corsOptions = {
+  origin: "*", // Allow all origins
+  methods: "GET, POST, PUT, DELETE, OPTIONS",
+  allowedHeaders: "Content-Type, Authorization",
+};
+
+app.use(cors(corsOptions));
+
 // Initialize app
 const app = express();
-
-// Middlewares
-app.use(
-  cors({
-    origin: "https://taikospin-frontend.vercel.app", // Allow only your frontend
-    methods: "GET,POST,PUT,DELETE,OPTIONS", // Allowed HTTP methods
-    allowedHeaders: "Content-Type,Authorization", // Allowed headers
-  })
-);
 app.use(bodyParser.json()); // Parse JSON request bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 
