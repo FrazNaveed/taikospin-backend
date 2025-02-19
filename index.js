4797fdc7b2793962301fac556733dd42056d0f3b
Fraz Naveed <01-134181-021@student.bahria.edu.pk>
2025-02-19 06:35:31 +0500

    fix: cors removed from index

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const bodyParser = require("body-parser");
 const authRoutes = require("./routes/auth");
 const contractRoutes = require("./routes/contract");
 
 const app = express(); // Initialize app first
-
-const corsOptions = {
-  origin: "*", // Allow all origins
-  methods: "GET, POST, PUT, DELETE, OPTIONS",
-  allowedHeaders: "Content-Type, Authorization",
-  credentials: true, // Allow credentials (if needed)
-  optionsSuccessStatus: 204, // Respond successfully to preflight requests
-  preflightContinue: false,
-};
-
-app.use(cors(corsOptions)); // Place this before routes
 
 app.use(bodyParser.json()); // Parse JSON request bodies
 app.use(bodyParser.urlencoded({ extended: true })); // Parse URL-encoded request bodies
